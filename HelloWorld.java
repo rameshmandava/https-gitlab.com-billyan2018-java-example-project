296720135e7c9e27a0220e3ebf0d6be9730f5341
Grant Tarrant-Fisher <grant@chaosiq.io>
2020-11-09 18:38:12 +0000

    Add new file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // do nothing 
+    }
+
+    public int hello(int a, int b, int c, int d, int e, int f, int j, int k)
+    {
+        return a + b
+    }
+
+}
