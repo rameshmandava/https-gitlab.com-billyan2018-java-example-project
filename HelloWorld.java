619c38250a92f689a18dd430eb55ce299e8e7604
Grant Tarrant-Fisher <grant@chaosiq.io>
2020-11-10 08:03:05 +0000

    fix println

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.prinln("Hello, World");
+        System.out.println("Hello, World");
     }
 
     public int hello(int a, int b, int c, int d)
