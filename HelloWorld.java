a08b6bb4045ffa4977c91b16a5f0decfe3e8f58d
Grant Tarrant-Fisher <grant@chaosiq.io>
2020-11-10 08:00:10 +0000

    add hello world print out

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // do nothing 
+        System.out.prinln("Hello, World");
     }
 
     public int hello(int a, int b, int c, int d)
