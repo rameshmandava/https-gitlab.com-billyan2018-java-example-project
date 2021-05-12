b291a792349135ef5706ea19d7ef3f7059235ef3
Bill Yan <bill.libin.yan@kingland.com>
2021-05-12 03:08:11 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     static int shared = 0;
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World   ");
     }
 
     public int hello(int a, int b, int c, int d, int e, int f, int g)
